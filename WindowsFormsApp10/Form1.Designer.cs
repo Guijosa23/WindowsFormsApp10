40e7ee2b83399e9d0fd1fa50e953d47b9ee480b1
Guijosa23 <mariguij553@ed.amdsb.ca>
2017-10-16 13:38:17 -0400

    Add project files.

---
+++ WindowsFormsApp10/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace WindowsFormsApp10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.paellaLabel = new System.Windows.Forms.Label();
+            this.chorizoLabel = new System.Windows.Forms.Label();
+            this.paellaTextbox = new System.Windows.Forms.TextBox();
+            this.chorizoTextbox = new System.Windows.Forms.TextBox();
+            this.vinoLabel = new System.Windows.Forms.Label();
+            this.vinoTextbox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.printButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(1, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(450, 142);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(1, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(447, 169);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(4, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(447, 133);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Red;
+            this.label4.Font = new System.Drawing.Font("Juice ITC", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(53, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 73);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Casa";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Red;
+            this.label5.Font = new System.Drawing.Font("Juice ITC", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(152, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(172, 124);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Pepe";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // paellaLabel
+            // 
+            this.paellaLabel.BackColor = System.Drawing.Color.Yellow;
+            this.paellaLabel.Font = new System.Drawing.Font("Lucida Handwriting", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paellaLabel.Location = new System.Drawing.Point(12, 165);
+            this.paellaLabel.Name = "paellaLabel";
+            this.paellaLabel.Size = new System.Drawing.Size(168, 32);
+            this.paellaLabel.TabIndex = 5;
+            this.paellaLabel.Text = "Paella Valenciana";
+            this.paellaLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // chorizoLabel
+            // 
+            this.chorizoLabel.BackColor = System.Drawing.Color.Yellow;
+            this.chorizoLabel.Font = new System.Drawing.Font("Lucida Handwriting", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chorizoLabel.Location = new System.Drawing.Point(1, 210);
+            this.chorizoLabel.Name = "chorizoLabel";
+            this.chorizoLabel.Size = new System.Drawing.Size(168, 32);
+            this.chorizoLabel.TabIndex = 6;
+            this.chorizoLabel.Text = "Typical chorizo";
+            this.chorizoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // paellaTextbox
+            // 
+            this.paellaTextbox.Location = new System.Drawing.Point(200, 177);
+            this.paellaTextbox.Name = "paellaTextbox";
+            this.paellaTextbox.Size = new System.Drawing.Size(79, 20);
+            this.paellaTextbox.TabIndex = 7;
+            // 
+            // chorizoTextbox
+            // 
+            this.chorizoTextbox.Location = new System.Drawing.Point(200, 217);
+            this.chorizoTextbox.Name = "chorizoTextbox";
+            this.chorizoTextbox.Size = new System.Drawing.Size(79, 20);
+            this.chorizoTextbox.TabIndex = 8;
+            // 
+            // vinoLabel
+            // 
+            this.vinoLabel.BackColor = System.Drawing.Color.Yellow;
+            this.vinoLabel.Font = new System.Drawing.Font("Lucida Handwriting", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vinoLabel.Location = new System.Drawing.Point(12, 252);
+            this.vinoLabel.Name = "vinoLabel";
+            this.vinoLabel.Size = new System.Drawing.Size(168, 32);
+            this.vinoLabel.TabIndex = 9;
+            this.vinoLabel.Text = "Vino Crianza 2008";
+            this.vinoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // vinoTextbox
+            // 
+            this.vinoTextbox.Location = new System.Drawing.Point(200, 259);
+            this.vinoTextbox.Name = "vinoTextbox";
+            this.vinoTextbox.Size = new System.Drawing.Size(79, 20);
+            this.vinoTextbox.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Yellow;
+            this.label7.Font = new System.Drawing.Font("Lucida Handwriting", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(280, 141);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(168, 22);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Prices:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Yellow;
+            this.label8.Font = new System.Drawing.Font("Lucida Handwriting", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(313, 165);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 32);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "$18.00";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Yellow;
+            this.label9.Font = new System.Drawing.Font("Lucida Handwriting", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(332, 210);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(62, 32);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "$9.00";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.Yellow;
+            this.label10.Font = new System.Drawing.Font("Lucida Handwriting", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(323, 252);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(89, 32);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "$48.00";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.priceLabel.Location = new System.Drawing.Point(63, 321);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(331, 67);
+            this.priceLabel.TabIndex = 15;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcButton.Location = new System.Drawing.Point(184, 285);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(111, 33);
+            this.calcButton.TabIndex = 16;
+            this.calcButton.Text = "CALCULATE";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // printButton
+            // 
+            this.printButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.printButton.Location = new System.Drawing.Point(160, 400);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(153, 33);
+            this.printButton.TabIndex = 17;
+            this.printButton.Text = "PRINT RECEIPT";
+            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 433);
+            this.Controls.Add(this.printButton);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.vinoTextbox);
+            this.Controls.Add(this.vinoLabel);
+            this.Controls.Add(this.chorizoTextbox);
+            this.Controls.Add(this.paellaTextbox);
+            this.Controls.Add(this.chorizoLabel);
+            this.Controls.Add(this.paellaLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label paellaLabel;
+        private System.Windows.Forms.Label chorizoLabel;
+        private System.Windows.Forms.TextBox paellaTextbox;
+        private System.Windows.Forms.TextBox chorizoTextbox;
+        private System.Windows.Forms.Label vinoLabel;
+        private System.Windows.Forms.TextBox vinoTextbox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Button printButton;
+    }
+}
+
---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        //global constants
+        const double PAELLA = 18.00;
+        const double CHORIZO = 9.00;
+        const double VINO = 48.00;
+        const double TAX = 0.13;
+
+        //global variables
+        int numofPaellas = 0;
+        int numofChorizos = 0;
+        int numofVinos = 0;
+        double totalPrice = 0;
+        double subTotal = 0;
+        
+
+        
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                numofPaellas = Convert.ToInt16(paellaTextbox.Text);
+                numofChorizos = Convert.ToInt16(chorizoTextbox.Text);
+                numofVinos = Convert.ToInt16(vinoTextbox.Text);
+
+                double subTotal = numofPaellas * PAELLA + numofChorizos * CHORIZO + numofVinos
+                    * VINO;
+                double totalPrice = subTotal * TAX;
+                double total = subTotal * TAX + subTotal;
+
+                priceLabel.Text = "SubTotal              " + subTotal.ToString("C");
+                priceLabel.Text += "\nTax                      " + TAX.ToString("C");
+                priceLabel.Text += "\nTotal                    " + total.ToString("C");
+               
+            }
+            catch
+            {
+                priceLabel.Text = "ERROR";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double subTotal = numofPaellas * PAELLA + numofChorizos * CHORIZO + numofVinos
+                    * VINO;
+            double totalPrice = subTotal * TAX;
+            double total = subTotal * TAX + subTotal;
+            
+            
+
+            //blank screen
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+            label9.Visible = false;
+            label10.Visible = false;
+            priceLabel.Visible = false;
+            paellaLabel.Visible = false;
+            chorizoLabel.Visible = false;
+            vinoLabel.Visible = false;
+            paellaTextbox.Visible = false;
+            chorizoTextbox.Visible = false;
+            vinoTextbox.Visible = false;
+            calcButton.Visible = false;
+            printButton.Visible = false;
+
+
+            Graphics fg = this.CreateGraphics();
+            Font JuiceITC = new Font("Juice ITC", 32, FontStyle.Bold);
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+            Pen drawPen = new Pen(Color.Black, 7);
+            fg.FillRectangle(redBrush, 0, 0, 460, 145);
+            fg.FillRectangle(yellowBrush, 0, 145, 460, 155);
+            fg.FillRectangle(redBrush, 0, 300, 460, 145);
+            fg.DrawRectangle(drawPen, 72, 12, 310, 405);
+
+
+
+            //paper ticket
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+            fg.FillRectangle(drawBrush, 75, 15, 305, 400);
+
+
+            Font drawFont = new Font("Juice ITC", 32, FontStyle.Bold);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+
+            fg.DrawString("Casa", drawFont, blackBrush, 100, 55);
+
+            drawFont = new Font("Juice ITC", 66, FontStyle.Bold);
+            fg.DrawString("Pepe", drawFont, blackBrush, 165, 15);
+
+           
+
+            Thread.Sleep(500);
+            drawFont = new Font("Courier New", 12, FontStyle.Bold);
+            fg.DrawString("October 12, 2017", drawFont, blackBrush, 180, 117);
+            fg.DrawString("Order N*:  001", drawFont, blackBrush, 90, 140);
+            Thread.Sleep(500);
+
+            Thread.Sleep(500);
+            drawFont = new Font("Courier New", 11, FontStyle.Bold);
+            fg.DrawString("\nPaella        x" + numofPaellas + "....." + numofPaellas * 
+                PAELLA + " $", drawFont, blackBrush, 95, 155);
+            Thread.Sleep(1150);
+
+            Thread.Sleep(500);
+            fg.DrawString("\nChorizo       x" + numofChorizos + "....." + numofChorizos *
+              CHORIZO + " $", drawFont, blackBrush, 95, 180);
+            Thread.Sleep(500);
+
+            Thread.Sleep(500);
+            fg.DrawString("\nVino Botella  x" + numofVinos + "....." + numofVinos *
+             VINO + " $", drawFont, blackBrush, 95, 210);
+            Thread.Sleep(500)
+
+             Thread.Sleep(500);
+            fg.DrawString("SubTotal: ..........." + subTotal.ToString("C"), 
+                drawFont, blackBrush, 90, 270);
+             Thread.Sleep(500);
+
+            Thread.Sleep(500);
+            fg.DrawString("Tax:      ..........." + TAX.ToString("C"), 
+                drawFont, blackBrush, 90, 290);
+            Thread.Sleep(500);
+
+            Thread.Sleep(500);
+            fg.DrawString("SubTotal: ..........." + total.ToString("C"), 
+                drawFont, blackBrush, 90, 310);
+            Thread.Sleep(500);
+
+
+
+        }
+    }
+}
